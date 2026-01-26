b391d750dbb16907b182e6f1f5e5c431d0d06ce8
AlmaKaplan <alma20062010@gmail.com>
2026-01-26 20:33:01 +0200

    Added constants in current limits gear ....

--- src/main/java/frc/robot/Subsystems/Shooter/ShooterConstants.java
+++ src/main/java/frc/robot/Subsystems/Shooter/ShooterConstants.java
@@ -12,34 +12,41 @@ import frc.robot.PortMap;
 
 public class ShooterConstants {
 
-    public static final double FEED_FORWARD = 0;
-    public static final double EJECT_VELOCITY = 0;
-    public static final double IDLE_VELOCITY = 0;
-    public static final double WARMUP_VELOCITY = 0;
+        public static final double FEED_FORWARD = 0;
+        public static final double EJECT_VELOCITY = 0;
+        public static final double IDLE_VELOCITY = 0;
+        public static final double WARMUP_VELOCITY = 0;
 
-    public static final Motor MASTER_MOTOR = new Motor(PortMap.ShooterPorts.SHOOTER_MASTER, MotorType.KRAKEN,
-            "Shooter Master Motor", InvertedValue.Clockwise_Positive);
-    public static final Motor SLAVE_MOTOR = new Motor(PortMap.ShooterPorts.SHOOTER_SLAVE, MotorType.KRAKEN,
-            "Shooter Slave Motor", InvertedValue.Clockwise_Positive);
+        public static final double SHOOTER_MAX_VELOCITY = 0;
+        public static final double GEAR = 0;
+        public static final double TOLERANCE = 0;
+        public static final double CURRENT_LIMIT = 0;
+        public static final double STATOR_CURRENT_LIMIT = 0;
+        public static final double RAMP_RATE = 0.1;
 
-    private static final GainConfig REAL_GAIN_CONFIG = new GainConfig().withKP(0).withKI(0).withKD(0);
+        public static final Motor MASTER_MOTOR = new Motor(PortMap.ShooterPorts.SHOOTER_MASTER, MotorType.KRAKEN,
+                        "Shooter Master Motor", InvertedValue.Clockwise_Positive);
+        public static final Motor SLAVE_MOTOR = new Motor(PortMap.ShooterPorts.SHOOTER_SLAVE, MotorType.KRAKEN,
+                        "Shooter Slave Motor", InvertedValue.Clockwise_Positive);
 
-    public static VelocitySystemConstants shooterConstants = VelocitySystemConstants
-            .newBuilder("Shooter", REAL_GAIN_CONFIG, MASTER_MOTOR, SLAVE_MOTOR)
-            .gear(0)
-            .isBrake(false)
-            .tolerance(0)
-            .motorCurrentLimit(0)
-            .maxVelocity(0)
-            .statorCurrentLimit(true, 0)
-            .rampRate(0.1)
-            .build();
+        private static final GainConfig REAL_GAIN_CONFIG = new GainConfig().withKP(0).withKI(0).withKD(0);
 
-    public static final State IDLE = new State("IDLE");
-    public static final State SHOOTING = new State("SHOOTING");
-    public static final State WARMUP = new State("WARMUP");
-    public static final State FEEDING = new State("FEEDING");
-    public static final State STATIC_FEEDING = new State("STATIC_FEEDING");
-    public static final State EJECT = new State("EJECT");
+        public static VelocitySystemConstants shooterConstants = VelocitySystemConstants
+                        .newBuilder("Shooter", REAL_GAIN_CONFIG, MASTER_MOTOR, SLAVE_MOTOR)
+                        .gear(GEAR)
+                        .isBrake(false)
+                        .tolerance(TOLERANCE)
+                        .motorCurrentLimit(CURRENT_LIMIT)
+                        .maxVelocity(SHOOTER_MAX_VELOCITY)
+                        .statorCurrentLimit(true, STATOR_CURRENT_LIMIT)
+                        .rampRate(RAMP_RATE)
+                        .build();
+
+        public static final State IDLE = new State("IDLE");
+        public static final State SHOOTING = new State("SHOOTING");
+        public static final State WARMUP = new State("WARMUP");
+        public static final State FEEDING = new State("FEEDING");
+        public static final State STATIC_FEEDING = new State("STATIC_FEEDING");
+        public static final State EJECT = new State("EJECT");
 
 }
