e08f1e512177c78629331ee889dcc7725c243129
AmitayCarmi <amitaycarmi@gmail.com>
2026-01-26 21:47:53 +0200

    fixed Hood builder

--- src/main/java/frc/robot/Subsystems/Hood/HoodConstants.java
+++ src/main/java/frc/robot/Subsystems/Hood/HoodConstants.java
@@ -16,33 +16,35 @@ import frc.robot.PortMap;
 /** Add your docs here. */
 public class HoodConstants {
 
-    public static final double IDLE_POSITION = 0;
-    public static final double EJECT_POSITION = 0;
+        public static final double IDLE_POSITION = 0;
+        public static final double EJECT_POSITION = 0;
 
-    public static final double GEAR = 0;
-    public static final double POSITION_FACTOR = 0;
-    public static final double MOTOR_CURRENT_LIMIT = 0;
-    public static final double TOLERANCE = 0;
+        public static final double MIN_POSITION = 0;
+        public static final double MAX_POSITION = 0;
+        public static final double GEAR = 0;
+        public static final double POSITION_FACTOR = 0;
+        public static final double MOTOR_CURRENT_LIMIT = 0;
+        public static final double TOLERANCE = 0;
 
-    public static final Motor HOOD_MOTOR = new Motor(PortMap.HoodPorts.HOOD_MOTOR, MotorType.KRAKEN, "Hood Motor",
-            InvertedValue.Clockwise_Positive);
+        public static final Motor HOOD_MOTOR = new Motor(PortMap.HoodPorts.HOOD_MOTOR, MotorType.KRAKEN, "Hood Motor",
+                        InvertedValue.Clockwise_Positive);
 
-    private static final GainConfig REAL_GAIN_CONFIG = new GainConfig().withKP(0).withKI(0).withKD(0);
+        private static final GainConfig REAL_GAIN_CONFIG = new GainConfig().withKP(0).withKI(0).withKD(0);
 
-    static PositionSystemConstants hoodConstants = PositionSystemConstants
-            .newBuilder("Hood", REAL_GAIN_CONFIG, HOOD_MOTOR)
-            .positionFactor(POSITION_FACTOR)
-            .gear(GEAR)
-            .motorCurrentLimit(MOTOR_CURRENT_LIMIT)
-            .isBrake(true)
-            .statorCurrentLimit(true, MOTOR_CURRENT_LIMIT)
-            .tolerance(TOLERANCE)
-            .build();
+        static PositionSystemConstants hoodConstants = PositionSystemConstants
+                        .newBuilder("Hood", REAL_GAIN_CONFIG, HOOD_MOTOR)
 
-    public static final State IDLE = new State("IDLE");
-    public static final State SHOOTING = new State("SHOOTING");
-    public static final State FEEDING = new State("FEEDING");
-    public static final State STATIC_FEEDING = new State("STATIC_FEEDING");
-    public static final State EJECT = new State("EJECT");
+                        .gear(GEAR)
+                        .startPose(IDLE_POSITION)
+                        .isBrake(true)
+                        .range(MIN_POSITION, MAX_POSITION)
+                        .tolerance(TOLERANCE)
+                        .build();
+
+        public static final State IDLE = new State("IDLE");
+        public static final State SHOOTING = new State("SHOOTING");
+        public static final State FEEDING = new State("FEEDING");
+        public static final State STATIC_FEEDING = new State("STATIC_FEEDING");
+        public static final State EJECT = new State("EJECT");
 
 }
