f3f9bad80b9c689b2dfda3fda1cfc87edb47a195
GalDotan <93068735+GalDotan@users.noreply.github.com>
2026-01-26 22:00:13 +0200

    .

---
+++ src/main/java/frc/robot/RobotControl/SuperStructure.java
@@ -0,0 +1,101 @@
+
+package frc.robot.RobotControl;
+
+import com.MAutils.RobotControl.DeafultSuperStructure;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import frc.robot.RobotContainer;
+import frc.robot.Util.ShootingParameters;
+
+public class SuperStructure extends DeafultSuperStructure{ 
+    private static ShootingParameters currentShootingParameters;
+
+    public enum ShootingPreset {
+        CLOSE(10.0, 2000.0, new Pose2d()),
+        CLIMB(15.0, 2500.0, new Pose2d());
+
+        public final double hoodAngle;
+        public final double shooterRPM;
+        public final Pose2d pose;
+
+        private ShootingPreset (double hoodAngle, double shooterRPM, Pose2d pose) {
+            this.hoodAngle = hoodAngle;
+            this.shooterRPM = shooterRPM;
+            this.pose = pose;
+        }
+    }
+
+
+    public SuperStructure() {
+        super(null);
+    }
+
+    public static int getMainTagID() {
+        return 0;
+    }
+
+    public static double getTxToTarget() {
+        return 0.0;
+    }
+
+    public double getAbsAngleToTarget() {
+        return 0.0;
+    }
+
+    public double getAngleToFeeding() {
+        return 0.0;
+    }
+
+    public double getDistanceToFeeding() {
+        return 0.0;
+    }
+
+    public boolean isHittingNet() {
+        return false;
+    }
+
+    public boolean isFull() {
+        return false;
+    }
+
+    public boolean isBalls() {
+        return false;
+    }
+
+    public boolean isStuck() {
+        return false;
+    }
+
+    private double getShootingRPM(double distance) {
+        return 0.0;
+    } 
+
+    private double getHoodAngle(double distance) {
+        return 0.0;
+    }
+
+    public ShootingParameters getShootingParameters() {
+        return currentShootingParameters;
+    }
+
+    public ShootingParameters getFeedingParameters() {
+        return new ShootingParameters(getShootingRPM(0.0), getHoodAngle(0.0));
+    }
+
+    private double getDistanceToTarget() {
+        return 0.0;
+    }
+
+    public static void update() {
+        if (RobotContainer.getRobotState() == SHOOTING) {
+            currentShootingParameters = new ShootingParameters(getDistanceToTarget(), MOVING_MPS);
+        }
+    }
+
+
+
+
+
+
+
+}
