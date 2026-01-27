d0ff84638ccdc9463718cfa7989516fe3b4da90b
Alma Kaplan <almakaplan222@gmail.com>
2026-01-27 16:54:03 +0200

    added roller

--- src/main/java/frc/robot/Commands/RollerCommand.java
+++ src/main/java/frc/robot/Commands/RollerCommand.java
@@ -1,4 +1,55 @@
 
 package frc.robot.Commands;
 
-public class RollerCommand {}
+import com.MAutils.RobotControl.SubsystemCommand;
+
+import frc.robot.Subsystems.Roller.Roller;
+import frc.robot.Subsystems.Roller.RollerConstants;
+
+public class RollerCommand extends SubsystemCommand {
+    private static final Roller roller = Roller.getInstance();
+
+    public RollerCommand() {
+        super(roller);
+        addRequirements(roller);
+    }
+
+
+    @Override
+    public void Automatic() {
+        switch (roller.getCurrentState().stateName) {
+            case "IDLE":
+                roller.setVoltage(RollerConstants.IDLE_VOLTAGE);
+                break;
+            case "INTAKE":
+                roller.setVoltage(RollerConstants.INTAKE_VOLTAGE);
+                break;
+            case "FEEDING":
+                roller.setVoltage(RollerConstants.FEEDING_VOLTAGE);
+                break;
+            case "FEDDING_IN_MOTION":
+                roller.setVoltage(RollerConstants.FEDDING_IN_MOTION_VOLTAGE);
+                break;
+            case "EJECT":
+                roller.setVoltage(RollerConstants.FEEDING_EJECT_VOLTAGE);
+                break;
+            case "SHOOTING":
+                roller.setVoltage(RollerConstants.SHOOTING_VOLTAGE);
+                break;
+            case "UNSTUCK":
+                roller.setVoltage(RollerConstants.UNSTUCK_VOLTAGE);
+                break;
+        }
+    }
+
+    @Override
+    public void Manual() {
+        
+    }
+
+    @Override
+    public void CantMove() {
+        roller.setVoltage(0);
+    }
+
+}
--- src/main/java/frc/robot/PortMap.java
+++ src/main/java/frc/robot/PortMap.java
@@ -1,6 +1,7 @@
 
 package frc.robot;
 
+import com.MAutils.CanBus.CANBusID;
 import com.ctre.phoenix6.CANBus;
 
 public class PortMap {
@@ -8,6 +9,10 @@ public class PortMap {
     public class CAN_BUS {
         public static final CANBus RIO_BUS = new CANBus("rio");
         public static final CANBus CANIVORE_BUS = new CANBus("*");
+    }
+
+    public class RollerPorts {
+        public static final CANBusID ROLLER_MOTOR = new CANBusID(21, CAN_BUS.RIO_BUS);
     }
 
 
