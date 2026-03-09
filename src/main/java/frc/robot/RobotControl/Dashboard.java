df54b6151beb1754fcfb59b4c665da124a1bd0d7
AmitayCarmi <amitaycarmi@gmail.com>
2026-03-09 18:38:38 +0200

    added intake factor to dashboard

--- src/main/java/frc/robot/RobotControl/Dashboard.java
+++ src/main/java/frc/robot/RobotControl/Dashboard.java
@@ -6,7 +6,7 @@ import com.MAutils.DashBoard.Tunable;
 public class Dashboard {
     private static Tunable activeDisable = new Tunable(0, "/Dashboard/ActiveDisabled");
     private static Tunable isFirstShift = new Tunable(0, "/Dashboard/Is First Shift");
-
+    private static Tunable intakeFactor = new Tunable(1, "/Dashboard/Intake Factor");
 
     public Dashboard() {
         
@@ -19,5 +19,9 @@ public class Dashboard {
 
     public static boolean isFirstShift() {
         return isFirstShift.get() == 1;
+    }
+
+    public static double getIntakeFactor() {
+        return intakeFactor.get();
     }
 }
