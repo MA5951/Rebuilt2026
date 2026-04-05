02b2c5632d2f7ddffb7ff00420f551678936370f
GalDotan <93068735+GalDotan@users.noreply.github.com>
2026-04-05 13:35:51 +0300

    COMMIT

--- src/main/java/frc/robot/Commands/Auto/FeedingAuto.java
+++ src/main/java/frc/robot/Commands/Auto/FeedingAuto.java
@@ -4,7 +4,9 @@ package frc.robot.Commands.Auto;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.RobotConstants;
 import frc.robot.Subsystems.Swerve.SwerveAutoFollower;
 import frc.robot.Util.Field;
@@ -15,7 +17,10 @@ public class FeedingAuto extends SequentialCommandGroup {
         addCommands(
                 SwerveAutoFollower.followPath("F2"),
                 new InstantCommand(() -> RobotConstants.SHOOTING_UNLOCKED.setState()),
-                new LockTarget(Field.flipByAlliance(new Pose2d(3.121,0.89,new Rotation2d())), 0.1, true, 0.3)
+                new ParallelCommandGroup(new LockTarget(Field.flipByAlliance(new Pose2d(3.121,0.89,new Rotation2d())), 0.1, true, 0.3), new WaitCommand(6)),
+                new InstantCommand(() -> RobotConstants.INTAKE_DEPLOY.setState()),
+                new InstantCommand(() -> RobotConstants.IDLE_SHOOTER.setState()),
+                SwerveAutoFollower.followPath("F3")
         );
     }
 }
